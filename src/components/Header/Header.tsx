62534c9321c7770a018738aa809a866d28e36de0
Konstantin <konstantin.muenster@mail.com>
2023-07-10 14:02:29 +0200

    add mobile navigation

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,6 +1,7 @@
 'use client';
 
-import { ComponentProps } from 'react';
+import { ComponentProps, FC } from 'react';
+import { Menu } from 'lucide-react';
 import clsx from 'clsx';
 
 import { navigation } from '@/config/navigation.config';
@@ -9,23 +10,39 @@ import { useScrollPosition } from '@/hooks/useScrollPosition';
 import { Button } from '../Button';
 import { Logo } from '../Logo';
 import { ScrollableRelativeToParent } from '../Helpers';
+import { SlideOver } from '../Slideover';
 
 export const Header = () => {
   return (
     <header className="px-container mx-auto flex min-h-[calc(44px+2.5rem)] max-w-container justify-between gap-8 py-5">
       <Logo iconRotated textHidden />
-      <nav className="flex items-center gap-12">
-        {navigation.slice(1).map(({ name, href }) => {
-          return (
-            <Button as="a" variant="text" href={href} key={href}>
-              {name}
-            </Button>
-          );
-        })}
-        <CallToActionButton />
-      </nav>
-      <FixedCallToActionButton />
+      {/* DESKTOP */}
+      <div className="hidden md:block">
+        <NavigationItems className="flex items-center gap-8 lg:gap-12" />
+        <FixedCallToActionButton />
+      </div>
+      {/* MOBILE */}
+      <div className="block md:hidden">
+        <SlideOver Icon={Menu}>
+          <NavigationItems className="flex flex-col items-start gap-8" />
+        </SlideOver>
+      </div>
     </header>
+  );
+};
+
+const NavigationItems: FC<{ className?: string }> = props => {
+  return (
+    <nav className={props.className}>
+      {navigation.map(({ name, href }) => {
+        return (
+          <Button as="a" variant="text" href={href} key={href}>
+            {name}
+          </Button>
+        );
+      })}
+      <CallToActionButton className="mt-4 md:mt-0" />
+    </nav>
   );
 };
 
@@ -49,7 +66,7 @@ const FixedCallToActionButton = () => {
   );
 };
 
-const CallToActionButton = (props: ComponentProps<'a'>) => {
+const CallToActionButton: FC<ComponentProps<'a'>> = props => {
   return (
     <Button as="a" href="mailto:hey@konstantin.digital" size="sm" {...props}>
       hey@konstantin.digital
---
+++ src/components/Slideover/Slideover.tsx
@@ -0,0 +1,86 @@
+import { FC, Fragment, ReactNode, useState } from 'react';
+import { Dialog, Transition } from '@headlessui/react';
+import { LucideIcon, X } from 'lucide-react';
+import clsx from 'clsx';
+
+type SlideOverProps = {
+  children: ReactNode;
+  title?: string;
+  Icon: LucideIcon;
+};
+
+export const SlideOver: FC<SlideOverProps> = ({ children, title, Icon }) => {
+  const [open, setOpen] = useState(false);
+
+  return (
+    <>
+      <button type="button" onClick={() => setOpen(true)} className="p-1">
+        <span className="sr-only">Open panel</span>
+        <Icon size={32} />
+      </button>
+      <Transition.Root show={open} as={Fragment}>
+        <Dialog as="div" className="relative z-10" onClose={setOpen}>
+          <Transition.Child
+            as={Fragment}
+            enter="ease-in-out duration-250"
+            enterFrom="opacity-0"
+            enterTo="opacity-100"
+            leave="ease-in-out duration-250"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+          >
+            <div className="fixed inset-0 bg-black/5 transition-opacity" />
+          </Transition.Child>
+
+          <div className="fixed inset-0 overflow-hidden">
+            <div className="absolute inset-0 overflow-hidden">
+              <div className="pointer-events-none fixed inset-y-0 right-0 flex max-w-full pl-20">
+                <Transition.Child
+                  as={Fragment}
+                  enter="transform transition ease-in-out duration-250"
+                  enterFrom="translate-x-full"
+                  enterTo="translate-x-0"
+                  leave="transform transition ease-in-out duration-250"
+                  leaveFrom="translate-x-0"
+                  leaveTo="translate-x-full"
+                >
+                  <Dialog.Panel className="pointer-events-auto w-screen max-w-md">
+                    <div className="flex h-full flex-col overflow-y-scroll rounded-l-2xl bg-white py-6 shadow-xl">
+                      <div className="px-8">
+                        <div
+                          className={clsx(
+                            'flex items-start',
+                            title ? 'justify-between' : 'justify-end'
+                          )}
+                        >
+                          {title && (
+                            <Dialog.Title className="text-base font-semibold leading-6 text-black">
+                              {title}
+                            </Dialog.Title>
+                          )}
+                          <div className="ml-3 flex h-7 items-center">
+                            <button
+                              type="button"
+                              className="rounded-md bg-white text-black/50 hover:text-black"
+                              onClick={() => setOpen(false)}
+                            >
+                              <span className="sr-only">Close panel</span>
+                              <X className="h-8 w-8" aria-hidden="true" />
+                            </button>
+                          </div>
+                        </div>
+                      </div>
+                      <div className="relative mt-6 flex-1 px-8">
+                        {children}
+                      </div>
+                    </div>
+                  </Dialog.Panel>
+                </Transition.Child>
+              </div>
+            </div>
+          </div>
+        </Dialog>
+      </Transition.Root>
+    </>
+  );
+};
