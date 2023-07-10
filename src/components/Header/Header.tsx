057c048b4b70a922dfb43b447b7e9c3532ac3318
Konstantin <konstantin.muenster@mail.com>
2023-07-10 08:05:56 +0200

    make header button fixed

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -4,7 +4,7 @@ import { Logo } from '../Logo';
 
 export const Header = () => {
   return (
-    <header className="px-container mx-auto flex max-w-container justify-between gap-8 py-8">
+    <header className="px-container relative mx-auto flex max-w-container justify-between gap-8 py-5">
       <Logo iconRotated textHidden />
       <nav className="flex items-center gap-12">
         {/* {navigation.slice(1).map(({ name, href }) => {
@@ -14,9 +14,11 @@ export const Header = () => {
             </Button>
           );
         })} */}
-        <Button as="a" href="/contact" size="sm">
-          hey@konstantin.digital
-        </Button>
+        <div className="fixed right-[4.25rem] top-[1.75rem]">
+          <Button as="a" href="/contact" size="sm">
+            hey@konstantin.digital
+          </Button>
+        </div>
       </nav>
     </header>
   );
