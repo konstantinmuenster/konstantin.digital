aba4aeafaad592e01b13669efb35835fefd6b217
Konstantin <konstantin.muenster@mail.com>
2023-07-24 19:24:24 +0200

    add lighthouse config

---
+++ lighthouserc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  ci: {
+    collect: {
+      startServerCommand: 'npm run start',
+      startServerReadyPattern: 'ready on',
+      url: ['http://localhost:3000'],
+      numberOfRuns: 1,
+      settings: {
+        preset: 'desktop',
+      },
+    },
+    assert: {
+      preset: 'lighthouse:recommended',
+    },
+    upload: {
+      target: 'temporary-public-storage',
+    },
+    server: {},
+  },
+};
