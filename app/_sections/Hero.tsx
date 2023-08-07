f7a066af5618ecba2b428e590d65c4556de955f9
Konstantin <konstantin.muenster@mail.com>
2023-08-07 18:41:49 +0200

    improve hero section

--- app/_sections/Hero.tsx
+++ app/_sections/Hero.tsx
@@ -3,6 +3,7 @@ import { CheckCircle } from 'lucide-react';
 import { Button } from '@/components/Button';
 import { Hero } from '@/components/Hero';
 import { ThemeToggle } from '@/components/ThemeToggle';
+import { AvatarBubble } from '@/components/Avatar';
 
 export const HeroSection = () => {
   return (
@@ -18,22 +19,23 @@ export const HeroSection = () => {
 };
 
 const HeroTitle = (
-  <>
+  <div className="relative">
+    <AvatarBubble className="absolute -right-24 -top-12 hidden lg:block" />
     <span className="block text-violet">Writing code,</span>
     <span className="block">thinking product.</span>
-  </>
+  </div>
 );
 
 const HeroActions = (
   <div className="mt-6 flex-col items-center justify-center text-center">
     <Button as="a">Schedule idea sparring</Button>
-    <div className="mt-6 flex gap-4 text-sm opacity-50">
+    <div className="mb-12 mt-6 flex gap-4 text-sm opacity-50">
       <span className="flex items-center gap-1">
         <CheckCircle size={18} /> 15min call
       </span>
       <span className="flex items-center gap-1">
         <CheckCircle size={18} />
-        Get feedback on idea
+        Get feedback on your idea
       </span>
     </div>
   </div>
