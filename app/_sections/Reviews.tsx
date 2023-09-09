6c8b777603ba0601abcbf712a3ad02cf82954cdd
Konstantin <konstantin.muenster@mail.com>
2023-09-09 07:17:12 +0200

    update reviews

--- app/_sections/Reviews.tsx
+++ app/_sections/Reviews.tsx
@@ -21,13 +21,13 @@ export const ReviewsSection = () => {
 };
 
 const reviews = [
-  {
-    name: 'Ahmed Menouni',
-    bio: 'Co-Founder & CEO',
-    avatar: '/images/reviews/ahmed.jpeg',
-    text: "“You'd be hard-pressed to find someone as skilled and easy to work with as Konstantin.  Incredibly organized, methodical, and with an attention to detail, Konstantin is a rare engineer who brings a sense of design in addition to an ability to execute on technical requirements.”",
-    url: 'https://lazy.so/',
-  },
+  // {
+  //   name: 'Ahmed Menouni',
+  //   bio: 'Co-Founder & CEO',
+  //   avatar: '/images/reviews/ahmed.jpeg',
+  //   text: "“You'd be hard-pressed to find someone as skilled and easy to work with as Konstantin.  Incredibly organized, methodical, and with an attention to detail, Konstantin is a rare engineer who brings a sense of design in addition to an ability to execute on technical requirements.”",
+  //   url: 'https://lazy.so/',
+  // },
   {
     name: 'Peter Zimmerman',
     bio: 'Founder',
@@ -41,5 +41,12 @@ const reviews = [
     avatar: '/images/reviews/pete-m.jpeg',
     text: "“If you're looking for someone to partner with, who you can trust to look after not just your site design and implementation now, but also be able to adapt the design to your future needs, who uses open source off the shelf components where it makes sense, but can customise or create any feature (with great code quality) when necessary, who turns around complete sites quickly, and small tweaks even faster, then look no further.”",
     url: 'https://expressvpn.com/',
+  },
+  {
+    name: 'Brendan Horan',
+    bio: 'Distinguished Engineer',
+    avatar: '/images/reviews/brendan-h.jpeg',
+    text: '“Working with Konstantin was a very refreshing change. Konstantin from the very start ensured communications were sent out, and boards were updated. Tasks assigned and detailed steps of what he was doing or what he needed from me. Given we had to work in full async due to timezone differences not once did I feel I did not know the progress of the work. I would have no issues working with Konstantin again or recommending him to anyone. It was a lot of fun and in the end, I got exactly what I had in mind and more.”',
+    url: 'https://horan.hk/',
   },
 ];
