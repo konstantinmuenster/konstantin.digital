5c5f40b7ee4438f4e0a3aad32fae752cb4546350
Konstantin <konstantin.muenster@mail.com>
2023-10-14 22:19:23 +0200

    add preliminary project page

---
+++ app/projects/[slug]/page.tsx
@@ -0,0 +1,19 @@
+const Project = () => {
+  return (
+    <main className="px-container mx-auto max-w-container pb-24">
+      <h1>Hello</h1>
+    </main>
+  );
+};
+
+/** EXPORTS */
+
+// export const metadata: Metadata = {
+//   title: 'Case Studies',
+//   description: 'A collection of freelance and open-source projects.',
+//   alternates: {
+//     canonical: `https://konstantin.digital/projects`,
+//   },
+// };
+
+export default Project;
